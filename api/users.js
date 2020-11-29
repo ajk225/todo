2e0535a36531f5d5ef7e163ac3151f3a1a214c66
Andrew Koe <ajk225@gmail.com>
2020-11-28 22:39:04 -0500

    fix

--- api/users.js
+++ api/users.js
@@ -3,13 +3,13 @@ const express = require('express');
 const router = express.Router();
 
 const User = require('../models/user');
-router.get('/',(req,res) => {
+router.get('/api/users',(req,res) => {
     User.find()
         .then(users => res.json(users))
         .catch(err => console.log(err))
 })
 
-router .post('/', (req,res) =>{
+router .post('/api/users', (req,res) =>{
     const { name,email } = req.body;
     const newUser = new User({
         name: name, email: email
