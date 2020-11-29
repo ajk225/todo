bff70b6ca933a5f1614b07fadec8c083dd3a7a65
Andrew Koe <ajk225@gmail.com>
2020-11-28 21:54:34 -0500

    add app def

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
+const app = express();
 require('./database');
 
 app.use(bodyParser.json());
