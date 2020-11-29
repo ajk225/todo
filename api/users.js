3b2bc8d3fdc21276b1bc4709aa105df21bc6ea54
Andrew Koe <ajk225@gmail.com>
2020-11-28 22:08:16 -0500

    fix typo

--- api/users.js
+++ api/users.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-const User = require('../models/User');
+const User = require('../models/user');
 router.get('/',(req,res) => {
     User.find()
         .then(users => res.json(users))
