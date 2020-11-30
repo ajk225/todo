27cd93e8379f665b2f278a300ca4f3fca4892e3d
Andrew Koe <ajk225@gmail.com>
2020-11-29 19:58:58 -0500

    test

--- api/users.js
+++ api/users.js
@@ -3,13 +3,13 @@ const express = require('express');
 const router = express.Router();
 
 const User = require('../models/user');
-router.get('/api/users',(req,res) => {
+router.get('/',(req,res) => {
     User.find()
         .then(users => res.json(users))
         .catch(err => console.log(err))
 })
 
-router.post('/api/users', (req,res) =>{
+router.post('/', (req,res) =>{
     const { name,email } = req.body;
     const newUser = new User({
         name: name, email: email
